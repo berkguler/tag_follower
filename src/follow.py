5e18d8074ca4d6418c1cbdf92bab3ba82847762e
berkguler <gulerb15@itu.edu.tr>
2020-05-10 05:58:05 +0300

    remove double same tag filter

--- src/follow.py
+++ src/follow.py
@@ -38,24 +38,24 @@ def getBox(msg):
 		# tag = msg.markers[0].pose.pose.position
 		for i in range(0,len(msg.markers)):
 			print("%d - %.2f - %.2f - %.2f // %.2f - %.2f - %.2f - %.2f"%(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
-			if msg.markers[i].id not in readed_markers:
+			#if msg.markers[i].id not in readed_markers:
 				# print("%d bir tene var abey"%msg.markers[i].id)
-				readed_markers.append(msg.markers[i].id)
-				marker_list.append(TagClass(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
-			else:
-				index_ = readed_markers.index(msg.markers[i].id)
-				if marker_list[index_].ID == msg.markers[i].id:
-					# print("%d iki tene var abey"%msg.markers[i].id)
-					# print("%.2f + %.2f "%(marker_list[index_].x, msg.markers[i].pose.pose.position.x))
-					marker_list[index_].x = (marker_list[index_].x + msg.markers[i].pose.pose.position.x)/2
-					marker_list[index_].y = (marker_list[index_].y + msg.markers[i].pose.pose.position.y)/2
-					marker_list[index_].z = (marker_list[index_].z + msg.markers[i].pose.pose.position.z)/2
-					marker_list[index_].r = (marker_list[index_].r + msg.markers[i].pose.pose.orientation.x)/2
-					marker_list[index_].p = (marker_list[index_].p + msg.markers[i].pose.pose.orientation.y)/2
-					marker_list[index_].yaw = (marker_list[index_].yaw + msg.markers[i].pose.pose.orientation.z)/2
-					marker_list[index_].z = (marker_list[index_].w + msg.markers[i].pose.pose.orientation.w)/2
-					#print("Son durum %.2f "%(marker_list[index_].x))
-			
+			readed_markers.append(msg.markers[i].id)
+			marker_list.append(TagClass(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
+#			else:
+#				index_ = readed_markers.index(msg.markers[i].id)
+#				if marker_list[index_].ID == msg.markers[i].id:
+#					# print("%d iki tene var abey"%msg.markers[i].id)
+#					# print("%.2f + %.2f "%(marker_list[index_].x, msg.markers[i].pose.pose.position.x))
+#					marker_list[index_].x = (marker_list[index_].x + msg.markers[i].pose.pose.position.x)/2
+#					marker_list[index_].y = (marker_list[index_].y + msg.markers[i].pose.pose.position.y)/2
+#					marker_list[index_].z = (marker_list[index_].z + msg.markers[i].pose.pose.position.z)/2
+#					marker_list[index_].r = (marker_list[index_].r + msg.markers[i].pose.pose.orientation.x)/2
+#					marker_list[index_].p = (marker_list[index_].p + msg.markers[i].pose.pose.orientation.y)/2
+#					marker_list[index_].yaw = (marker_list[index_].yaw + msg.markers[i].pose.pose.orientation.z)/2
+#					marker_list[index_].z = (marker_list[index_].w + msg.markers[i].pose.pose.orientation.w)/2
+#					#print("Son durum %.2f "%(marker_list[index_].x))
+#			
 			if 1 in readed_markers:
 				target_index = readed_markers.index(1)
 				tag.x = marker_list[target_index].x
@@ -95,8 +95,8 @@ if __name__ == '__main__':
 		
 		#print(x)
 		y = x[0]
-		speed.linear.x = 0.09*(sqrt(pow((tag.x),2)+pow((tag.z),2)))
-		speed.angular.z = 0.1*tag.x*tag.z+0.7*y
+		speed.linear.x = 0.04*(sqrt(pow((tag.x),2)+pow((tag.z),2)))
+		speed.angular.z = 0.1*tag.x*tag.z+0.4*y
 		break_.linear.x = 0.0
 		break_.angular.z = 0.0
 
