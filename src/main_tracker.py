01897e27b580c03f61d05c28cbecb527b3557926
berkguler <gulerb15@itu.edu.tr>
2020-06-05 14:54:59 +0300

    a new approach

--- src/main_tracker.py
+++ src/main_tracker.py
@@ -5,11 +5,16 @@ import tf
 import actionlib
 import time
 
+import math
+import numpy as np
+
 from geometry_msgs.msg import Twist, Point, Quaternion
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from teleop import process as teleop_process
+from sensor_msgs.msg import LaserScan
+
+from tf import TransformListener
 
 goal_found = False
 
@@ -23,45 +28,154 @@ class TagClass:
 		self.p = p
 		self.yaw = yaw
 		self.w = w
-		self.item_list = [self.r, self.p, self.yaw, self.w]
 
-goal = MoveBaseGoal()
+class TagFollower:
 
-def getBox(msg):
-	global goal
-	global goal_found
-	global marker_list
-	marker_list = []
-	readed_markers = []
-	goal = MoveBaseGoal()
-	rospy.loginfo("ariyorum")
-	if len(msg.markers) >= 1:
-		# tag = msg.markers[0].pose.pose.position
-		for i in range(0,len(msg.markers)):
+	def __init__ (self):
+		self.goal = MoveBaseGoal()
+		self.readed_markers = []
+		self.readed_markers_tf = []
+		self.marker_list= []
+		self.tf = TransformListener()
+		rospy.loginfo("tagFollower Class initalized")
+
+	def tagScan(self):
+		self.tag_msg = rospy.wait_for_message("/ar_pose_marker",AlvarMarkers,timeout=None)
+		self.getBox(self.tag_msg)
+
+	def getBox(self,msg):
+		if len(msg.markers) >= 1:
+			""" Is there a readed tags?"""
+			for i in range(0,len(msg.markers)):
+				""" Each readed tags """
+				print("%d - %.2f - %.2f - %.2f // %.2f - %.2f - %.2f - %.2f"%(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
+				if not msg.markers[i].id in self.readed_markers:
+					""" Has it been readed? """
+					self.readed_markers.append(msg.markers[i].id) # add readed_markers list
+					self.tag_frame = "/ar_marker_" + str(msg.markers[i].id) # create the frame
+					self.old_quaternion = (
+						msg.markers[i].pose.pose.orientation.x,
+						msg.markers[i].pose.pose.orientation.y,
+						msg.markers[i].pose.pose.orientation.z,
+						msg.markers[i].pose.pose.orientation.w
+					)
+					
+					self.old_offset = (
+						msg.markers[i].pose.pose.position.x,
+						msg.markers[i].pose.pose.position.y,
+						msg.markers[i].pose.pose.position.z,
+					)
+
+					self.new_translation, self.new_quaternion =  self.RotateTags(self.old_offset,self.old_quaternion,'x')
+
+
+					self.marker_list.append(TagClass(msg.markers[i].id,self.new_translation[0],self.new_translation[1],self.new_translation[2],self.new_quaternion[0],self.new_quaternion[1],self.new_quaternion[2],self.new_quaternion[3]))
+					self.readed_markers_tf.append(self.moveARtag2Base(self.tag_frame))
+
+					self.getTagFrame(self.tag_frame)
+
+	def RotateTags(self,translation,orientation,axis):
+		if axis == 'y':
+			self.rotation_matrix =  np.array([0,0,-1,0,1,0,1,0,0]).reshape(3,3) #-90 degree about the y axis
+		elif axis == 'x':
+			self.rotation_matrix =  np.array([1,0,0,0,0,1,0,-1,0]).reshape(3,3) #90 degree about the x axis
+		self.converted_trans = np.dot(self.rotation_matrix,np.asarray(translation).reshape(3,1))
+		self.converted_euler = np.dot(self.rotation_matrix,np.asarray(tf.transformations.euler_from_quaternion(orientation)).reshape(3,1))
+		self.converted_quaternion = tf.transformations.quaternion_from_euler(self.converted_euler[0], self.converted_euler[1],self.converted_euler[2])
+		print("Translation %s\n------------------------"%axis)
+		print(translation)
+		print(self.converted_trans)
+		print("Orientation %s\n------------------------"%axis)
+		print(tf.transformations.euler_from_quaternion(orientation))
+		print(self.converted_euler)
+		return self.converted_trans, self.converted_quaternion
+		
+
+	def getHuskyPose(self):	
+		try:
+			(trans,rot) = self.tf.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+			print("\nHusky Pose Detected!")
+			homo_matrix = tf.transformations.quaternion_matrix(rot)
+			homo_matrix[0][3] = trans[0]
+			homo_matrix[1][3] = trans[1]
+			homo_matrix[2][3] = trans[2]
+			print(homo_matrix)
+			print(tf.transformations.euler_from_quaternion(rot))
+			print("\n")
+			return (trans,rot,homo_matrix)
 			
-			print("%d - %.2f - %.2f - %.2f // %.2f - %.2f - %.2f - %.2f"%(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
-			readed_markers.append(msg.markers[i].id)
-			marker_list.append(TagClass(msg.markers[i].id,msg.markers[i].pose.pose.position.x,msg.markers[i].pose.pose.position.y,msg.markers[i].pose.pose.position.z,msg.markers[i].pose.pose.orientation.x,msg.markers[i].pose.pose.orientation.y,msg.markers[i].pose.pose.orientation.z,msg.markers[i].pose.pose.orientation.w))
-
-			if 1 in readed_markers:
-				goal_found = True
-				target_index = readed_markers.index(1)
-				goal.target_pose.header.frame_id = "map"
-				goal.target_pose.header.stamp = rospy.Time.now()
-				goal.target_pose.pose.position.x = marker_list[target_index].x
-				goal.target_pose.pose.position.y = marker_list[target_index].y
-				goal.target_pose.pose.position.z = marker_list[target_index].z
-				goal.target_pose.pose.orientation.x = marker_list[target_index].yaw
-				goal.target_pose.pose.orientation.y = -1*marker_list[target_index].p
-				goal.target_pose.pose.orientation.z = marker_list[target_index].r
-				goal.target_pose.pose.orientation.w = marker_list[target_index].x
-				print(goal)
-			else: 
-				goal = MoveBaseGoal()
-	else:
-		goal = MoveBaseGoal()
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			print("Olmadi birader")		
 
 
+	def getTagFrame(self,tagFrame):	
+		try:
+			(trans,rot) = self.tf.lookupTransform('/base_footprint', tagFrame, rospy.Time(0))
+			print("\n%s  tag frame Detected!"%tagFrame)
+			print(trans)
+			print(tf.transformations.euler_from_quaternion(rot))
+			
+			new_trans, new_rot = self.RotateTags(trans,rot,'x')
+			homo_matrix = tf.transformations.quaternion_matrix(new_rot)
+			homo_matrix[0][3] = new_trans[0]
+			homo_matrix[1][3] = new_trans[1]
+			homo_matrix[2][3] = new_trans[2]
+			return (new_trans,new_rot,homo_matrix)
+		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+			print("Olmadi birader")		
+
+
+	def moveARtag2Base(self,tagframe):
+		return np.dot(self.getTagFrame(tagframe)[2],self.getHuskyPose()[2])
+
+
+	def gotoBox(self):
+		print("Available markers:\n" + str(self.readed_markers))
+		if len(self.readed_markers) >= 1:
+			rospy.loginfo("You can start the process")
+			self.target_tag = int(raw_input("enter the ID of the target box: "))
+			if self.target_tag in self.readed_markers:
+			 	self.target_index = self.readed_markers.index(self.target_tag)
+				self.target_homo_matrix = self.readed_markers_tf[self.target_index]
+			 	self.goal.target_pose.header.frame_id = "map"
+			 	self.goal.target_pose.header.stamp = rospy.Time.now()
+				self.target_orientation = tf.transformations.quaternion_from_matrix(self.target_homo_matrix)
+			 	self.goal.target_pose.pose.position.x = self.target_homo_matrix[0][3]
+			 	self.goal.target_pose.pose.position.y = self.target_homo_matrix[1][3]
+			  	self.goal.target_pose.pose.position.z = self.target_homo_matrix[2][3]
+				self.goal.target_pose.pose.orientation.x = self.target_orientation[0]
+			 	self.goal.target_pose.pose.orientation.y = self.target_orientation[1]
+			 	self.goal.target_pose.pose.orientation.z = self.target_orientation[2]
+			 	self.goal.target_pose.pose.orientation.w = self.target_orientation[3]
+			 	print(self.goal)
+				self.result = movebase_client(self.goal)
+				if self.result:
+					rospy.loginfo("Goal Execution Done!")
+				else:
+					rospy.loginfo("There is nothing to follow")
+	
+	def search(self):
+		self.waypoints = [(0,-2, 0),(0,2,math.pi),(0,-2,math.pi),(0,2,0),(0,0,0)]
+		for point in self.waypoints:
+			self.goal.target_pose.header.frame_id = "map"
+			self.goal.target_pose.header.stamp = rospy.Time.now()
+			self.goal.target_pose.pose.position.x = point[0] 
+			self.goal.target_pose.pose.position.y = point[1]
+			self.quat = quaternion_from_euler(0,0,point[2])
+			self.goal.target_pose.pose.orientation.x = self.quat[0]
+			self.goal.target_pose.pose.orientation.y = self.quat[1]
+			self.goal.target_pose.pose.orientation.z = self.quat[2]
+			self.goal.target_pose.pose.orientation.w = self.quat[3]
+			
+			print(self.goal)
+			self.result = movebase_client(self.goal)
+			if self.result:
+				rospy.loginfo("Goal Execution Done!")
+			else:
+				rospy.loginfo("There is nothing to follow")
+			self.tagScan()
+		print(self.readed_markers)
+		  	
 
 def movebase_client(target_goal):
 	# Create an action client called "move_base" with action definition file "MoveBaseAction"
@@ -72,27 +186,60 @@ def movebase_client(target_goal):
 	print("Goal sended")
 	wait = client.wait_for_result()
 
-	
 	if not wait:
 		rospy.logerr("Action server not available!")
 		rospy.signal_shutdown("Action server not available!")
 	else:
 		return client.get_result()
 
+def random_walk(x,y):
+
+	goal = MoveBaseGoal()
+	goal.target_pose.header.frame_id = "map"
+	goal.target_pose.header.stamp = rospy.Time.now()
+	goal.target_pose.pose.position.x = x
+	goal.target_pose.pose.position.y = y
+	goal.target_pose.pose.orientation.w = 1
+	result = movebase_client(goal)
+	if result:
+			rospy.loginfo("Goal Execution Done!")
+
+def getLaser(laser_msg):
+	# values at 0 degree
+	print laser_msg.ranges[0]
+	# values at 90 degree
+	print laser_msg.ranges[360]
+	# values at 180 degree
+	print laser_msg.ranges[719]
+
 
 if __name__ == '__main__':
 	try:
 		rospy.init_node("tag_follower")
-		msg = rospy.wait_for_message("/ar_pose_marker",AlvarMarkers,timeout=None)
-		rospy.loginfo("bunu goruyon mu gotelek")
-		teleop_process
-		getBox(msg)
-		if(goal_found):
-			result = movebase_client(goal)
-			
-			if result:
-				rospy.loginfo("Goal Execution Done!")
-		else:
-			rospy.loginfo("yarrak")
+		move = rospy.Publisher("/husky_velocity_controller/cmd_vel",Twist, queue_size = 10 )
+		tagMonster = TagFollower()
+		while not rospy.is_shutdown():
+			choice = int(raw_input("What do you want?\n(-1) - break \n(1) - scan tags \n(2) - go-to-goal\n(3) - follow tag \n:"))
+			if choice == 1:
+				#scan Laser
+				laser_msg = rospy.wait_for_message("/robot_arm/laser/scan",LaserScan)
+				getLaser(laser_msg)
+				tagMonster.search()
+				
+			elif choice == 2:
+				x = float(raw_input("x: "))
+				y = float(raw_input("y: "))
+				random_walk(x,y)
+				tagMonster.tagScan()
+			elif choice == 3:
+
+				tagMonster.gotoBox()
+			elif choice == 4:
+				tagMonster.getHuskyPose()
+			elif choice == 9:
+				tagMonster.tagScan()
+			elif choice == -1:
+				break
+
 	except rospy.ROSInterruptException:
 		rospy.loginfo("Navigation test finished.")
--- src/teleop.py
+++ src/teleop.py
@@ -44,7 +44,9 @@ def on_release(key):
         print("quit on PAUSE")
         return False
 
-def process():
+
+
+if __name__=='__main__':
 	rospy.init_node ("tag_follower")
 	move = rospy.Publisher("/husky_velocity_controller/cmd_vel",Twist, queue_size = 10 )
 	rate = rospy.Rate(100)
@@ -52,13 +54,10 @@ def process():
 	velocity = Point()
 	listener = Listener(on_press=on_press, on_release=on_release, suppress=False)
 	listener.start()	
-	#print("A Hybrid Mobile Manipulator Teleop Interface\n------------------------------------------------------------\n\t\tUp Arrow\n\t\tLinear Speed Up\nLeft Arrow\t\tDown Arrow\t\tRight Arrow\nTurn Left\t\tLinear Speed Down\t\tTurn Right\n\nCtrl_r\t\tStop\nShift_l\t\increase incrementation\nCtrl_l\t\treduce incrementation\nShift_r\t\tReset incrementation\n\n")
+	print("A Hybrid Mobile Manipulator Teleop Interface\n------------------------------------------------------------\n\t\tUp Arrow\n\t\tLinear Speed Up\nLeft Arrow\t\tDown Arrow\t\tRight Arrow\nTurn Left\t\tLinear Speed Down\t\tTurn Right\n\nCtrl_r\t\tStop\nShift_l\t\increase incrementation\nCtrl_l\t\treduce incrementation\nShift_r\t\tReset incrementation\n\n")
 	while listener.running and not rospy.is_shutdown():
 		rospy.loginfo(speed)
 		speed.linear.x = velocity.x
 		speed.angular.z = velocity.z
 		move.publish(speed)
 		rate.sleep()
-		
-if __name__=='__main__':
-	process()
