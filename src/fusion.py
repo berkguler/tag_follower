bb9b6112f6eca9cb95cd28b5b9d65da39fdc3418
berkguler <gulerb15@itu.edu.tr>
2020-06-12 17:46:40 +0300

    comparing wrt base_footprint

--- src/fusion.py
+++ src/fusion.py
@@ -16,43 +16,43 @@ from sensor_msgs.msg import LaserScan
 from tf import TransformListener
 
 
-class TagClass:
-	def __init__(self,ID,x,y,z,r,p,yaw,w):
-		self.ID = ID
-		self.x = x
-		self.y = y
-		self.z = z
-		self.r = r
-		self.p = p
-		self.yaw = yaw
-		self.w = w
 
 class TagMatcher:
 
-    def __init__ (self):
-        self.tf = TransformListener()
-        rospy.loginfo("TagMatcher Initalized!!!!!!")
+	def __init__ (self):
+		self.tf = TransformListener()
+		rospy.loginfo("TagMatcher Initalized!!!!!!")
 
 	def getMessages(self):
-		self.L_msg = rospy.wait_for_message("/datmo/box_kf",TrackArray,timeout=None)
-		self.T_msg = rospy.wait_for_message("/ar_pose_marker",AlvarMarkers,timeout=None)
+		L_msg = rospy.wait_for_message("/datmo/box_kf",TrackArray,timeout=None)
+		T_msg = rospy.wait_for_message("/ar_pose_marker",AlvarMarkers,timeout=None)
+		return L_msg, T_msg
 
-	def Fusion(lidar_message,tag_message):
-		# Rotate Tag
-		
-		# 2 /map
-		# read L_msg
-		# compare
+	def Fusion(self):
+		L_msg, T_msg = self.getMessages()
+		if len(T_msg.markers) >= 1:
+			for i in range(0,len(T_msg.markers)):
+				for k in range(0, len(L_msg.tracks)):
+					# Tag
+					self.tag_frame_name = "/ar_marker_" + str(T_msg.markers[i].id)
+					self.tag_trans, self.tag_orient, self.tag_matrix = self.getTagFrame(self.tag_frame_name)
+					# Box
+					box_x = L_msg.tracks[k].odom.pose.pose.position.x
+					box_y = L_msg.tracks[k].odom.pose.pose.position.y
+					box_orientation = (
+						L_msg.tracks[k].odom.pose.pose.orientation.x,
+						L_msg.tracks[k].odom.pose.pose.orientation.y,
+						L_msg.tracks[k].odom.pose.pose.orientation.z,
+						L_msg.tracks[k].odom.pose.pose.orientation.w
+					)
+					box_euler = tf.transformations.euler_from_quaternion(box_orientation)
+					circle = math.sqrt(math.pow(self.tag_trans[0] - box_x,2)+math.pow(self.tag_trans[1] - box_y,2))
+					if circle <= 1 :
+						print("-!-!--!-!---!--!---!--!--!-!--!-!--\nID: %d\nCamera:\t%.2f\t%.2f\nLidar:\t%.2f\t%.2f\nCircle: %.2f\t%.2f"%(T_msg.markers[i].id,self.tag_trans[0],self.tag_trans[1],box_x,box_y,circle,box_euler[2]))
+					else:
+						
+						print("Olmadi") # -----------------------\nID: %d\nCamera:\t%.2f\t%.2f\nLidar:\t%.2f\t%.2f\nCircle: %.2f"%(T_msg.markers[i].id,self.tag_trans[0],self.tag_trans[1],box_x,box_y,circle))
 
-	def getTagMessage(self,msg):
-		if len(msg.markers) >= 1:
-			for i in range(0,len(msg.markers)):
-				
-
-    # def getBoxfromLidar(self,msg):
-    #     for i in range(0,len(msg.tracks)):
-    #        if  msg.tracks[i].odom.pose.pose.position.x > 4.0 and msg.tracks[i].odom.pose.pose.position.x < 6.0:
-    #            print msg.tracks[i]
 
 	def Rotation(self,translation,orientation):
 		self.rotation_matrix =  np.array([1,0,0,0,0,-1,0,1,0]).reshape(3,3) #-90 degree about the x axis
@@ -61,10 +61,7 @@ class TagMatcher:
 		self.converted_quaternion = tf.transformations.quaternion_from_euler(self.converted_euler[0], self.converted_euler[1],self.converted_euler[2])
 		return self.converted_trans, self.converted_quaternion
 
-	def moveARtag2Base(self,tagframe):
-		return np.dot(self.getTagFrame(tagframe)[2],self.getHuskyPose()[2])
-
-    def getHuskyPose(self):	
+	def getHuskyPose(self):	
 		try:
 			(trans,rot) = self.tf.lookupTransform('/map', '/base_footprint', rospy.Time(0))
 			homo_matrix = tf.transformations.quaternion_matrix(rot)
@@ -75,7 +72,7 @@ class TagMatcher:
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			print("Error Occured! - Detection of Husky Pose wrt map frame")		
 
-    def getTagFrame(self,tagFrame):	
+	def getTagFrame(self,tagFrame):	
 		try:
 			(trans,rot) = self.tf.lookupTransform('/base_footprint', tagFrame, rospy.Time(0))
 			new_trans, new_rot = self.Rotation(trans,rot)
@@ -86,9 +83,16 @@ class TagMatcher:
 			return (new_trans,new_rot,homo_matrix)
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			print("Error Occured! - Detection of Tag Pose wrt base_footprint frame")	
+		
+	def moveARtag2Base(self,tagframe):
+		return np.dot(self.getTagFrame(tagframe)[2],self.getHuskyPose()[2])		
 
 
 if __name__ == '__main__':
-    rospy.init_node("fusion")
-    matcher = TagMatcher()
+	rospy.init_node("fusion")
+	r = rospy.Rate(1)
+	matcher = TagMatcher()
+	while not rospy.is_shutdown():
+		matcher.Fusion()
+		r.sleep()
  
